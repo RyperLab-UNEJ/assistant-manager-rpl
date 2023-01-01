51a61fb8f7cb06e9d39bb0351d64c87db3c7c335
ZaidanNur <zaixyz332@gmail.com>
2023-01-01 08:48:55 +0700

    create cms create daftar kompetisi

--- app/Http/Livewire/Cms/Competition/CreateCompetition.php
+++ app/Http/Livewire/Cms/Competition/CreateCompetition.php
@@ -2,20 +2,31 @@
 
 namespace App\Http\Livewire\Cms\Competition;
 
-use App\Models\CompetitionLevel;
-use App\Models\Participant;
 use Livewire\Component;
+use App\Models\Competition;
+use App\Models\Participant;
+use App\Models\CompetitionLevel;
 
 class CreateCompetition extends Component
 {
+
+    // define eloquent model to use direct data binding
+    public Competition $competition;
+
     public $participants;
     public $competition_level;
-    public $peserta = [];
-    public $remember;
+    public $status = ['Active','Non Active'];
 
     // POST
-    public $participant_id;
-    public $competition_level_id;
+    protected $rules = [
+        'competition.participant_id'=>'required|integer',
+        'competition.competition_level_id'=>'required|integer',
+        'competition.competition_name'=>'required|string',
+        'competition.begin_date'=>'required|date',
+        'competition.end_date'=>'required|date',
+        'competition.status'=>'nullable',
+    ];
+
     public function render()
     {
         return view('livewire.cms.competition.create-competition')
@@ -26,5 +37,22 @@ class CreateCompetition extends Component
     public function mount(){
         $this->participants = Participant::pluck('participant_name','id');
         $this->competition_level = CompetitionLevel::pluck('level_name','id');
+        $this->competition = new Competition();
+    }
+
+    public function backToIndex()
+    {
+        return redirect(route('cms.competition.index'));
+    }
+
+    public function save()
+    {
+        $this->validate();
+
+        $this->competition->save();
+
+        session()->flash('success', 'New competition successfully created.');
+
+        return redirect(route('cms.competition.index'));
     }
 }
--- resources/views/livewire/cms/competition/create-competition.blade.php
+++ resources/views/livewire/cms/competition/create-competition.blade.php
@@ -6,15 +6,18 @@
 <div class="card">
         <div class="card-body">
             <h4 class="card-title">Tambah Kompetisi Baru</h4>
-            <form class="forms-sample">
-                <x-cms.select title="Partisipan" wireModel='participant_id' :options="$participants"/>
-                <x-cms.text title="Username" placeholder="Write your username .."/>
-                <x-cms.text title="Email" type="email" placeholder="Write your email .."/> 
-                <x-cms.text title="Password" type="password" placeholder="Write your password .."/> 
-                <x-cms.text title="Confirm Password" type="password" placeholder="Write your password .."/> 
-                <x-cms.checkbox title="Remember Me" wireModel='remember'/> 
-                <button type="submit" class="btn btn-primary mr-2">Submit</button>
-                <button class="btn btn-light">Cancel</button>
+            <form class="forms-sample" wire:submit.prevent="save">
+                <x-cms.select title="Partisipan" wireModel='competition.participant_id' :options="$participants"/>
+                <x-cms.select title="Competition Level" wireModel='competition.competition_level_id' :options="$competition_level"/>
+                <x-cms.text title="Competition Name" wireModel='competition.competition_name' placeholder="Write competition name here .."/>
+                <x-cms.select title="Status" wireModel='competition.status' :options="$status"/>
+                <x-cms.text title="Begin Date" type="date" wireModel='competition.begin_date'/>
+                <x-cms.text title="End Date" type="date" wireModel='competition.end_date' />
+
+
+
+                <button type="submit" class="btn btn-primary mr-2" >Submit</button>
+                <button type="button" class="btn btn-light" wire:click='backToIndex'>Cancel</button>
             </form>
             </div>
         </div>
