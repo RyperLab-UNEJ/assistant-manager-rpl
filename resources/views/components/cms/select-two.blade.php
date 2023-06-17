72dec08981fed10c348c6a5d6f9ba2c72c731621
ZaidanNur <zaixyz332@gmail.com>
2023-06-17 22:24:06 +0700

    adjustment select2 component

--- resources/views/components/cms/select-two.blade.php
+++ resources/views/components/cms/select-two.blade.php
@@ -1,18 +1,11 @@
-@section('component-script')
-
-<script>
-    $(document).ready(function () {
-        $('#select2').select2();
-        $('#select2').on('change', function (e) {
-            var data = $('#select2').select2("val");
-        @this.set({{ $wireModel }}, data);
-        });
-    });
-</script>
-@endsection
-<div class="form-group flex flex-col" wire:ignore>
+<div class="form-group flex flex-col"  x-data="{value: @entangle($wireModel)}"
+    x-init="$nextTick(() => {
+            select2 = $('#select2').select2();
+            select2.on('select2:select', (event) => {value = event.target.value});
+            })" wire:ignore>
     <label class="{{ $labelClass }}" for="{{ $id }}">{{ $title }}</label>
-    <select wire:model='{{ $wireModel }}' class="form-control form-control-lg {{ $inputClass }}" style="font-size: 16px" id="{{ $id }}" name="{{ $name }}" {{ $addAttributes }}>
+    <select x-ref="select"
+    x-bind:value="value" wire:model='{{ $wireModel }}' class="form-control form-control-lg {{ $inputClass }}" style="font-size: 16px" id="select2" name="{{ $name }}" {{ $addAttributes }}>
         <option>{{ $firstOption }}</option>
         @foreach ($options as $key=>$value)
             <option value="{{ $key }}">{{ $value }}</option>
